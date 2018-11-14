84ade25b48354c03884437cb7a56dbb2454ef6d0
RiceDemolisher <taran.jhattu@gmail.com>
2018-11-14 17:11:53 +0100

    -Taran

--- 4.2/javascript.js
+++ 4.2/javascript.js
@@ -1,10 +1,10 @@
 var plaatjes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var teller = 0;
-var slideholder = document.getElementById("slideholder");
-slideholder.style.background = "url('img/aap1.jpg')";
+var slideholder1 = document.getElementById("slideholder1");
+slideholder1.style.background = "url('img/aap1.jpg')";
 
-slideholder.addEventListener("click", function () {
-    slideholder.style.background = "url('img/aap" + getAap() + ".jpg')";
+slideholder1.addEventListener("click", function () {
+    slideholder1.style.background = "url('img/aap" + getAap() + ".jpg')";
 });
 
 function getAap() {
